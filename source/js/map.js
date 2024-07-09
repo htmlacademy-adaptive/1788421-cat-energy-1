fd4436dee9cdb0f51cf4bede27ea6795f47fe803
ToryaToria <torya.toria@yandex.ru>
2024-07-09 10:51:16 +0300

    изменение размера маркера карты

--- source/js/map.js
+++ source/js/map.js
@@ -1,35 +1,58 @@
 document.querySelector('.main-footer__map').classList.remove('no-js');
 
-
 ymaps.ready(function () {
-  var myMap = new ymaps.Map('map', {
-    center: [59.938631, 30.323037],
-    zoom: 15
-  }, {
-    searchControlProvider: 'yandex#search'
-  }),
 
-    // Создаём макет содержимого.
-    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-    ),
+  // массив размеров маркера
+    const sizePlacemark = [
+        [57, 53],
+        [113, 106]
+    ];
 
-    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-      hintContent: 'Собственный значок метки',
-      balloonContent: 'ул. Большая Конюшенная, д. 19/8, </b><br/>' + ' Санкт-Петербург'
-    }, {
-      // Опции.
-      // Необходимо указать данный тип макета.
-      iconLayout: 'default#image',
-      // Своё изображение иконки метки.
-      iconImageHref: '../img/tmp/map-pin.png',
-      // Размеры метки.
-      iconImageSize: [113, 106],
-      // Смещение левого верхнего угла иконки относительно
-      // её "ножки" (точки привязки).
-      // iconImageOffset: [5, 38]
-    });
+      // массив масштабов
+    const myMapZoom = [11, 12, 15];
 
-  myMap.geoObjects
-    .add(myPlacemark);
+    var myMap = new ymaps.Map('map', {
+            // center: [59.938051, 30.323037],
+            center: [59.968528, 30.317632],
+
+    // изменения масштаба в зависимости от экрана
+            zoom: window.innerWidth <= 500 ? myMapZoom[0] : myMapZoom[1]
+        });
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'ул. Большая Конюшенная, д. 19/8',
+            balloonContent: '<b>ул. Большая Конюшенная, д. 19/8,</b><br/>' + ' Санкт-Петербург'
+        }, {
+
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+
+            // Своё изображение иконки метки.
+            iconImageHref: '../img/map-pin.png',
+
+    // Размеры метки в зависимости от размера экрана
+            iconImageSize: window.innerWidth <= 500 ? sizePlacemark[0] : sizePlacemark[1],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            // iconImageOffset: [155, 38]
+        });
+
+    myMap.geoObjects
+        .add(myPlacemark);
+
+        let check = true;
+        myMap.events.add('boundschange', function () {
+            if (window.innerWidth <= 500 && check) {
+                myPlacemark.options.set('iconImageSize', sizePlacemark[0]);
+                myMap.setZoom(myMapZoom[0], {duration: 300});
+                return check = false;
+            } else if (window.innerWidth > 500 && !check) {
+                myPlacemark.options.set('iconImageSize', sizePlacemark[1]);
+                myMap.setZoom(myMapZoom[1], { duration: 300 });
+                return check = true;
+            } else if (window.innerWidth > 900) {
+                myMap.setZoom(myMapZoom[2], { duration: 300 });
+            }
+        });
 });
