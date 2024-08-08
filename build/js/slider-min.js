aabbbb5ed742100266033dd06f7053ed41e1273f
ToryaToria <torya.toria@yandex.ru>
2024-08-08 13:48:08 +0300

    удалила build

--- build/js/slider-min.js
+++
@@ -1,5 +0,0 @@
-const container = document.querySelector('.slider');
-document.querySelector('.slider__range').addEventListener('input', (e) => {
-container.style.setProperty ('--position', `${e.target.value}%`);
-})
-console.log('Привет!');
