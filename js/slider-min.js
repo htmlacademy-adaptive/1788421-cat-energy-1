814f5ea96be319bff5cb83098d021ab0203bfcc4
keksobot <keksobot@users.noreply.github.com>
2024-09-03 10:10:45 +0000

    Deploying to gh-pages from @ htmlacademy-adaptive/1788421-cat-energy-1@1fbf9818acb3361a605f34f9906dc0a6cc9c4e6c 🚀

---
+++ js/slider-min.js
@@ -0,0 +1,1 @@
+const container=document.querySelector(".slider");document.querySelector(".slider__range").addEventListener("input",(e=>{container.style.setProperty("--position",`${e.target.value}%`)})),console.log("Привет!");
